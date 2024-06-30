690652df496c4a4a5d7c9fed10af7da97d8b7682
Adrien Bird <adrien@adrienbird.com>
2024-06-30 15:16:52 -0500

    fix: saved medusa config file

--- medusa-config.js
+++ medusa-config.js
@@ -35,7 +35,14 @@ const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
 
 const plugins = [
   `medusa-fulfillment-manual`,
-  `medusa-payment-manual`,
+  {
+    resolve: `medusa-payment-stripe`,
+    options: {
+      api_key: process.env.STRIPE_API_KEY,
+      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+    },
+  },
+  // `medusa-payment-manual`,
   {
     resolve: 'medusa-file-r2',
     options: {
